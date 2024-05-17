72959cfb00024890209cfdcdc0600701fc346ede
mysoyul <vega2k_@naver.com>
2024-05-17 15:57:04 +0900

    EditUser 수정

--- src/features/users/EditUser.tsx
+++ src/features/users/EditUser.tsx
@@ -1,40 +1,64 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
+// import { useDispatch, useSelector } from "react-redux";
 
 import TextField from "components/TextField";
 import Button from "components/Button";
 import User from "types/User";
-import { editUser } from "store/slice/usersSlice"
-import { RootState, AppDispatch } from "store";
+// import { editUser } from "store/slice/usersSlice"
+// import { RootState, AppDispatch } from "store";
+import { useGetUserByIdQuery, useUpdateUserMutation } from "store/service/usersApi";
+import LoadingSpinner from "components/LoadingSpinner";
 
 const EditUser = () => {
   const navigate = useNavigate();
   const params = useParams();
+  
+  const userId = params.id as string;
+  const [values, setValues] = useState<User>({
+    name: "",
+    email: "",
+  });
 
   // const usersState = useSelector((state: RootState) => state.users)
   // const users = usersState.userList;
-  const users: User[] = [
-      { id: "1", name: "Jane", email: "jane@email.com" },
-      { id: "2", name: "John", email: "john@email.com" },
-    ];
-  const existingUser = users.find(user => user.id === params.id);
-  const { name, email } = existingUser as User;
-  const [values, setValues] = useState<User>({
-    // name: name,
-    // email: email,
-    name, email
-  });
+  // const existingUser = users.find(user => user.id === params.id);
+  // const { name, email } = existingUser as User;
+  // const [values, setValues] = useState<User>({
+  //   // name: name,
+  //   // email: email,
+  //   name, email
+  // });
+  // const dispatch = useDispatch<AppDispatch>();
 
-  const dispatch = useDispatch<AppDispatch>();
+  const { data, isError, isLoading } = useGetUserByIdQuery(userId);
+  useEffect(() => {
+    setValues({ name: data?.name as string, email: data?.email as string });
+  }, [data]);
+
+  const [ updateUser ] = useUpdateUserMutation();
+
+	if( isLoading ) {
+    return <div><LoadingSpinner /></div>
+  }
+
+  if (isError || !data) {
+    return <div>User Not Found</div>;
+  }
 
   const handleEditUser = () => {
-    dispatch(editUser({
-      id: params.id,
-      name: values.name,
-      email: values.email
-    }));
-    setValues({ name: "", email: "" });
+    // dispatch(editUser({
+    //   id: params.id,
+    //   name: values.name,
+    //   email: values.email
+    // }));
+    updateUser({
+      id: userId,
+      data: {
+        name: values.name,
+        email: values.email,
+      },
+    });
     navigate("/");
   };
 
