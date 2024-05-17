b3b0f4f4d114c0fbc932608c9478320435df7af2
mysoyul <vega2k_@naver.com>
2024-05-17 15:29:07 +0900

    usersApi getUsers 호출,LoadingSpinner add

--- src/features/users/UserList.tsx
+++ src/features/users/UserList.tsx
@@ -1,23 +1,35 @@
 import { Link } from "react-router-dom";
-import { useSelector, useDispatch } from 'react-redux';
+//import { useSelector, useDispatch } from 'react-redux';
 
 import Button from "components/Button";
 import User from "types/User";
-import { RootState } from 'store';
-import { deleteUser } from "store/slice/usersSlice"
-import { AppDispatch } from "store";
+import { useGetUsersQuery } from "store/service/usersApi";
+import LoadingSpinner from "components/LoadingSpinner";
+// import { RootState } from 'store';
+// import { deleteUser } from "store/slice/usersSlice"
+// import { AppDispatch } from "store";
 
 const UserList = () => {
   // const users: User[] = [
   //   { id: "1", name: "Jane", email: "jane@email.com" },
   //   { id: "2", name: "John", email: "john@email.com" },
   // ];
-  const usersState = useSelector((state: RootState) => state.users)
-  const users = usersState.userList;
-  const dispatch = useDispatch<AppDispatch>();
+  // const usersState = useSelector((state: RootState) => state.users)
+  // const users = usersState.userList;
+  // const dispatch = useDispatch<AppDispatch>();
+
+  const { data, isError, isLoading } = useGetUsersQuery();
+  const users = data as User[];
+  if( isLoading ) {
+    return <div><LoadingSpinner /></div>
+  }
+
+  if (isError || !data) {
+    return <div>Something went wrong</div>;
+  }
 
   const handleRemoveUser = (id: string) => {
-    dispatch(deleteUser({ id } as User));
+    // dispatch(deleteUser({ id } as User));
   }
 
   const renderCard = () =>
@@ -42,7 +54,7 @@ const UserList = () => {
               <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                 <path strokeLinecap="round" strokeLinejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
               </svg>
-            </button>
+          </button>
         </div>
       </div>
     ));
